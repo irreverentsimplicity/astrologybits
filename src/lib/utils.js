8256cd01ca380bfbb860d965b6546ce72268aec3
irreverentsimplicity <irreverentsimplicity@proton.me>
2025-10-19 03:28:40 -0400

    accessiblity

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,7 @@
 export function makeLinksRelative(content) {
   if (!content) return '';
   
-  return content
+  let processed = content
     // Fix internal page links (but NOT images)
     .replace(/href=["']https?:\/\/astrologybits\.com\//g, 'href="/')
     .replace(/href=["']https?:\/\/www\.astrologybits\.com\//g, 'href="/')
@@ -10,4 +10,27 @@ export function makeLinksRelative(content) {
     // Update image URLs to use wp.astrologybits.com
     .replace(/src=["'](https?:\/\/)?astrologybits\.com\//g, 'src="https://wp.astrologybits.com/')
     .replace(/src=["'](https?:\/\/)?www\.astrologybits\.com\//g, 'src="https://wp.astrologybits.com/');
+  
+  // Add default alt text to images that don't have it
+  processed = processed.replace(/<img([^>]*)>/g, (match, attrs) => {
+    // Check if alt attribute exists
+    if (!attrs.includes('alt=')) {
+      // Extract title or use filename for alt text
+      let altText = 'Astrology related image';
+      const titleMatch = attrs.match(/title=["']([^"']+)["']/);
+      if (titleMatch) {
+        altText = titleMatch[1];
+      } else {
+        const srcMatch = attrs.match(/src=["']([^"']+)["']/);
+        if (srcMatch) {
+          const filename = srcMatch[1].split('/').pop().split('.')[0];
+          altText = filename.replace(/[-_]/g, ' ');
+        }
+      }
+      return `<img${attrs} alt="${altText}">`;
+    }
+    return match;
+  });
+  
+  return processed;
 }
